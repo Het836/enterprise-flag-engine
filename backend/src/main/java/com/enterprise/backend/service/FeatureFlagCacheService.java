08d7fc6cfb7cb866130d0f2e5ab0363fe9fac1c3
Vir Khemka <167968094+KBlad@users.noreply.github.com>
2026-07-14 23:49:37 +0530

    feat: implement Resilience4j circuit breaker for Redis fallback

--- backend/src/main/java/com/enterprise/backend/service/FeatureFlagCacheService.java
+++ backend/src/main/java/com/enterprise/backend/service/FeatureFlagCacheService.java
@@ -1,4 +1,5 @@
 package com.enterprise.backend.service;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -22,15 +23,28 @@ public class FeatureFlagCacheService {
         String key = CACHE_PREFIX + flagName;
 
         // Save it, but give it a Time-To-Live (TTL) of 24 hours just in case
-        redisTemplate.opsForValue().set(key, flagData, Duration.ofHours(24));
-        System.out.println("Saved flag to Redis: " + key);
+        try {
+            redisTemplate.opsForValue().set(key, flagData, Duration.ofHours(24));
+            System.out.println("Saved flag to Redis: " + key);
 
-        publisher.broadcastInvalidation(flagName);
+            publisher.broadcastInvalidation(flagName);
+        }catch (Exception e){
+            System.err.println("Redis is down. Could not save L2 cache");
+        }
     }
-
+    @CircuitBreaker(name = "redisCache", fallbackMethod = "fallbackGetFlag")
     // Method to fetch a flag from Redis L2 Cache
     public Object getFlagFromCache(String flagName) {
         String key = CACHE_PREFIX + flagName;
         return redisTemplate.opsForValue().get(key);
+    }
+    // The Fallback Method (The Safety Net)
+    // The signature must match the original method, but with an Exception parameter at the end.
+    public Object fallbackGetFlag(String flagName, Throwable throwable) {
+        System.err.println("🛡️ Circuit Breaker Tripped! Redis is unreachable. Bypassing L2 cache for: " + flagName);
+
+        // TODO: In the final version, this is where you ask Track A's L1 Cache or PostgreSQL for the data.
+        // For now, we return null so the app doesn't crash.
+        return null;
     }
 }
